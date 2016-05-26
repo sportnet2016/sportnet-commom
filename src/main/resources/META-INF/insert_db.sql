db5aa654f1adfb6713fbc4e48d1daa422bb05a3e
eder.lrbranco1 <eder.lrbranco1@CAS419604W0730.senacsp.edu.br>
2016-05-25 21:52:36 -0300

    Tabela BD com venda

--- src/main/resources/META-INF/insert_db.sql
+++ src/main/resources/META-INF/insert_db.sql
@@ -3,10 +3,12 @@ INSERT INTO TB_CATEGORIA (NM_CATEGORIA) VALUES
 ('Roupas'),
 ('Esportes');
 
-INSERT INTO TB_PRODUTO (NM_PRODUTO, DS_PRODUTO, DT_CADASTRO, VL_PRODUTO, QTD_ATUAL) VALUES
-('Tenis Nike Basquete', 'Indicado para jogar basquete', CURRENT_TIMESTAMP, 100.00, 10),
-('Tenis Nike Casual', 'Indicado para dia a dia', CURRENT_TIMESTAMP, 120.00, 20),
-('Tenis Asics Sport', 'Indicado para correr', CURRENT_TIMESTAMP, 90.00, 25);
+INSERT INTO TB_PRODUTO (NM_PRODUTO,ID_MARCA, GN_PRODUTO, DS_PRODUTO,TM_PRODUTO,COR_PRODUTO, DT_CADASTRO, VL_PRODUTO, QTD_ATUAL,ID_CATEGORIA)
+VALUES
+('Tenis Nike Basquete', 1,'masculino', 'Indicado para jogar basquete','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10,1),
+('Tenis Nike Casual', 1,'masculino', 'Indicado para andar','medio','azul', CURRENT_TIMESTAMP, 100.00, 10,1),
+('Tenis Asics Sport', 1,'masculino', 'Indicado para balada huehue','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10,1);
+
 
 INSERT INTO TB_IMAGEM (NM_IMAGEM, DS_IMAGEM, ID_PRODUTO) VALUES 
 ('tenis1.png', 'Tenis Nike 1', 1),
@@ -17,3 +19,14 @@ INSERT INTO TB_PRODUTO_CATEGORIA (ID_PRODUTO, ID_CATEGORIA) VALUES
 (1, 1),
 (2, 1),
 (3, 1);
+
+SELECT * fROM TB_CATEGORIA
+SELECT * fROM TB_MARCA
+SELECT * fROM TB_PRODUTO
+
+
+INSERT INTO TB_MARCA(NOME_MARCA)
+VALUES ('Nike');
+INSERT INTO TB_CATEGORIA(NM_CATEGORIA)
+VALUES ('Tenis');
+
