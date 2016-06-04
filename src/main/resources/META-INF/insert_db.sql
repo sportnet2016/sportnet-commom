cc513fcd10b6b000c4874357eb282e5b558e454b
Eder Rodrigues <Eder Rodrigues@Home>
2016-06-04 16:08:21 -0300

    Listando categoria em detalhe, TB_PRODUTO_CATEGORIA

--- src/main/resources/META-INF/insert_db.sql
+++ src/main/resources/META-INF/insert_db.sql
@@ -8,11 +8,11 @@ INSERT INTO TB_CATEGORIA (NM_CATEGORIA) VALUES
 ('Roupas'),
 ('Esportes');
 
-INSERT INTO TB_PRODUTO (NM_PRODUTO,ID_MARCA, GN_PRODUTO, DS_PRODUTO,TM_PRODUTO,COR_PRODUTO, DT_CADASTRO, VL_PRODUTO, QTD_ATUAL,ID_CATEGORIA)
+INSERT INTO TB_PRODUTO (NM_PRODUTO,ID_MARCA, GN_PRODUTO, DS_PRODUTO,TM_PRODUTO,COR_PRODUTO, DT_CADASTRO, VL_PRODUTO, QTD_ATUAL)
 VALUES
-('Tenis Nike Basquete', 1,'masculino', 'Indicado para jogar basquete','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10,1),
-('Tenis Nike Casual', 1,'masculino', 'Indicado para andar','medio','azul', CURRENT_TIMESTAMP, 100.00, 10,1),
-('Tenis Asics Sport', 1,'masculino', 'Indicado para balada huehue','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10,1);
+('Tenis Nike Basquete', 1,'masculino', 'Indicado para jogar basquete','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10),
+('Tenis Nike Casual', 1,'masculino', 'Indicado para andar','medio','azul', CURRENT_TIMESTAMP, 100.00, 10),
+('Tenis Asics Sport', 1,'masculino', 'Indicado para balada huehue','Grande','preto', CURRENT_TIMESTAMP, 100.00, 10);
 
 
 INSERT INTO TB_IMAGEM (NM_IMAGEM, DS_IMAGEM, ID_PRODUTO) VALUES 
@@ -20,10 +20,10 @@ INSERT INTO TB_IMAGEM (NM_IMAGEM, DS_IMAGEM, ID_PRODUTO) VALUES
 ('tenis2.png', 'Tenis Nike 2', 2),
 ('tenis3.png', 'Tenis Asics 3', 3);
 
--- INSERT INTO TB_PRODUTO_CATEGORIA (ID_PRODUTO, ID_CATEGORIA) VALUES 
--- (1, 1),
--- (2, 1),
--- (3, 1);
+INSERT INTO TB_PRODUTO_CATEGORIA (ID_PRODUTO, ID_CATEGORIA) VALUES 
+(1, 1),
+(2, 1),
+(3, 1);
 -- 
 -- SELECT * fROM TB_CATEGORIA
 -- SELECT * fROM TB_MARCA
